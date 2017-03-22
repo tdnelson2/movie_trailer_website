166c5249394648cdecbbaf27c14af85334a98b72
Tim Nelson <tdnelson@gmail.com>
2017-03-22 13:13:22 -0700

    Add video module with a class for storing information about videos of any kind

---
+++ video.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+class Video():
+    """Data structure for videos hosted on youtube"""
+
+    def __init__(self, title, description, thumb, link):
+        print("Video constructor initiated")
+        self. title =               title
+        self.description =          description
+        self.thumb =                thumb
+        self.link =                 link
+
+    def show_video(self):
+            webbrowser.open(self.link)
