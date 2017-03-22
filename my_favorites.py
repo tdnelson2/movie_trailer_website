bba27149d0e64a2c2ed124541265b65c746f75a9
Tim Nelson <tdnelson@gmail.com>
2017-03-22 15:50:30 -0700

    Remove TV Show modual with use only movies

--- my_favorites.py
+++ my_favorites.py
@@ -4,8 +4,4 @@ import tvshow
 toy_story = movie.Movie("Toy Story", "Toys come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=4KPTXpQehio")
-this_is_us = tvshow.TV_Show("This Is Us", "A dramedy about a millennial family",
-                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgzMTcyMTE0MF5BMl5BanBnXkFtZTgwOTk1ODY5MDI@._V1_.jpg",
-                            "https://www.youtube.com/watch?v=MQ86ycli47w",
-                            "http://titantv.com/search/search.aspx?stxt=this+is+us")
 this_is_us.show_video()
