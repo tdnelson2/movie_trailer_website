847c678762530446a467163f085dc4f854d11add
Tim Nelson <tdnelson@gmail.com>
2017-03-23 16:34:35 -0700

    Remove uneeded movie_storyline

--- movie.py
+++ movie.py
@@ -3,12 +3,10 @@ class Movie():
 
     """Data structure for movies"""
 
-	# Init using the same variable names fresh_tomatoes.py will look for
-    def __init__(self, title, movie_storyline,
-                 poster_image_url, trailer_youtube_url):
+	# Init using the variable names fresh_tomatoes.py will look for
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
 
         self. title = title
-        self.movie_storyline = movie_storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
--- my_favorites.py
+++ my_favorites.py
@@ -1,99 +1,56 @@
-# Bring in the movie module containing the Movie class
 import movie
-
-# Contains the functionality for creating the movie trailer website
 import fresh_tomatoes
 
 
-# Create an instance of Movie class for each title
-arrival = movie.Movie("Arrival", "When twelve mysterious spacecraft appear"
-                      " around the world, linguistics professor Louise Banks"
-                      " is tasked with interpreting the language of the"
-                      " apparent alien visitors.",
+# Create objects for each title
+arrival = movie.Movie("Arrival",
                       "https://upload.wikimedia.org/wikipedia/en/d/df/Arrival%2C_Movie_Poster.jpg",  # NOQA
                       "https://www.youtube.com/watch?v=ZLO4X6UI8OY")
 
-finding_nemo = movie.Movie("Finding Nemo", "After his son is captured in the"
-                           " Great Barrier Reef and taken to Sydney, a timid"
-                           " clownfish sets out on a journey to bring"
-                           " him home.",
+finding_nemo = movie.Movie("Finding Nemo",
                            "https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg",  # NOQA
                            "https://www.youtube.com/watch?v=SPHfeNgogVs")
 
-la_la_land = movie.Movie("La La Land", "A jazz pianist falls for an aspiring"
-                         " actress in Los Angeles.",
+la_la_land = movie.Movie("La La Land",
                          "https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png",  # NOQA
                          "https://www.youtube.com/watch?v=0pdqf4P9MB8")
 
-passengers = movie.Movie("Passengers", "A spacecraft traveling to a distant"
-                         " colony planet and transporting thousands of people"
-                         " has a malfunction in its sleep chambers."
-                         " As a result, two passengers are awakened"
-                         " 90 years early.",
+passengers = movie.Movie("Passengers",
                          "https://upload.wikimedia.org/wikipedia/en/8/8e/Passengers_2016_film_poster.jpg",  # NOQA
                          "https://www.youtube.com/watch?v=7BWWWQzTpNU")
 
-petes_dragon = movie.Movie("Pete's Dragon", "The adventures of an orphaned boy"
-                           " named Pete and his best friend Elliot, who just"
-                           " so happens to be a dragon.",
+petes_dragon = movie.Movie("Pete's Dragon",
                            "https://upload.wikimedia.org/wikipedia/en/d/d2/Petes_dragon_2016_film_poster.jpg",  # NOQA
                            "https://www.youtube.com/watch?v=fPOamb6d_20")
 
-wilderpeople = movie.Movie("Hunt for the Wilderpeople", "A national manhunt is"
-                           " ordered for a rebellious kid and his foster uncle"
-                           " who go missing in the wild New Zealand bush.",
+wilderpeople = movie.Movie("Hunt for the Wilderpeople",
                            "https://upload.wikimedia.org/wikipedia/en/4/44/Hunt_for_the_Wilderpeople.png",  # NOQA
                            "https://www.youtube.com/watch?v=lfT2uq_rNms")
 
-the_lobster = movie.Movie("The Lobster", "In a dystopian near future, single"
-                          " people, according to the laws of The City, are"
-                          " taken to The Hotel, where they are obliged to find"
-                          " a romantic partner in forty-five days or are"
-                          " transformed into beasts and sent off"
-                          " into The Woods.",
+the_lobster = movie.Movie("The Lobster",
                           "https://images-na.ssl-images-amazon.com/images/M/MV5BNDQ1NDE5NzQ1NF5BMl5BanBnXkFtZTgwNzA5OTM2NTE@._V1_UY268_CR3,0,182,268_AL_.jpg",  # NOQA
                           "https://www.youtube.com/watch?v=iFDncw9-1yw")
 
-the_martian = movie.Movie("The Martian", "An astronaut becomes stranded on"
-                          " Mars after his team assume him dead, and must rely"
-                          " on his ingenuity to find a way to signal to Earth"
-                          " that he is alive.",
+the_martian = movie.Movie("The Martian",
                           "https://upload.wikimedia.org/wikipedia/en/c/cd/The_Martian_film_poster.jpg",  # NOQA
                           "https://www.youtube.com/watch?v=ej3ioOneTy8&t=61s")
 
-the_revenant = movie.Movie("The Revenant", "A frontiersman on a fur trading"
-                           " expedition in the 1820s fights for survival after"
-                           " being mauled by a bear and left for dead by"
-                           " members of his own hunting team.",
+the_revenant = movie.Movie("The Revenant",
                            "https://upload.wikimedia.org/wikipedia/en/b/b6/The_Revenant_2015_film_poster.jpg",  # NOQA
                            "https://www.youtube.com/watch?v=LoebZZ8K5N0")
 
-mad_max = movie.Movie("Mad Max: Fury Road", "A woman rebels against a"
-                      " tyrannical ruler in postapocalyptic Australia in"
-                      " search for her home-land with the help of a group of"
-                      " female prisoners, a psychotic worshipper, and a"
-                      " drifter named Max.",
+mad_max = movie.Movie("Mad Max: Fury Road",
                       "https://upload.wikimedia.org/wikipedia/en/6/6e/Mad_Max_Fury_Road.jpg",  # NOQA
                       "https://www.youtube.com/watch?v=hEJnMQG9ev8")
 
-star_wars = movie.Movie("Star Wars: The Force Awakens", "Three decades after"
-                        " the defeat of the Galactic Empire, a new threat"
-                        " arises. The First Order attempts to rule the galaxy"
-                        " and only a ragtag group of heroes can stop them,"
-                        " along with the help of the Resistance.",
+star_wars = movie.Movie("Star Wars: The Force Awakens",
                         "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=sGbxmsDFVnE")
 
-bridge_of_spies = movie.Movie("Bridge of Spies", "During the Cold War, an"
-                              " American lawyer is recruited to defend an"
-                              " arrested Soviet spy in court, and then help"
-                              " the CIA facilitate an exchange of the spy for"
-                              " the Soviet captured American U2 spy plane"
-                              " pilot, Francis Gary Powers.",
+bridge_of_spies = movie.Movie("Bridge of Spies",
                               "https://upload.wikimedia.org/wikipedia/en/f/fa/Bridge_of_Spies_poster.jpg",  # NOQA
                               "https://www.youtube.com/watch?v=7JnC2LIJdR0")
 
-# Put each movie in an array for use in fresh_tomatoes.open_movies_page()
 movies = [arrival, finding_nemo, la_la_land, passengers, petes_dragon,
           wilderpeople, the_lobster, the_martian, the_revenant, mad_max,
           star_wars, bridge_of_spies]
