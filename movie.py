866ccfa0cd73a4df648ee133a8cee85f972c8126
Tim Nelson <tdnelson@gmail.com>
2017-03-23 11:05:43 -0700

    Remove Video Movie parent-child structure to simplify

--- movie.py
+++ movie.py
@@ -1,16 +1,15 @@
-import video
 
+class Movie():
 
-class Movie(video.Video):
-
-    """Data struction for movies"""
+    """Data structure for movies"""
 
 	# Init using the same variable names fresh_tomatoes.py will look for
-    def __init__(self, movie_title, movie_storyline,
-                 poster_image, trailer_youtube):
+    def __init__(self, title, movie_storyline,
+                 poster_image_url, trailer_youtube_url):
 
-        # Init the super-class
-        video.Video.__init__(self, movie_title, movie_storyline,
-                             poster_image, trailer_youtube)
+        self. title = title
+        self.movie_storyline = movie_storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
 
 
